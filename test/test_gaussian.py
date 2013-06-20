b61af208c14255215d7837ae4e3cb1f92d6ebb5f
Jerome Kieffer <jerome.kieffer@esrf.fr>
2013-06-20 18:18:57 +0200

    new gaussian genration with inplace reduction

--- test/test_gaussian.py
+++ test/test_gaussian.py
@@ -60,10 +60,12 @@ else:
     PROFILE = False
     queue = pyopencl.CommandQueue(ctx)
 
-kernel = "preprocess"
-kernel_file = os.path.join(os.path.dirname(os.path.abspath(sift.__file__)), kernel + ".cl")
-kernel_src = open(kernel_file).read()
-program = pyopencl.Program(ctx, kernel_src).build()
+kernels = {"preprocess":8 , "gaussian":512}
+for kernel in kernels:
+    kernel_file = os.path.join(os.path.dirname(os.path.abspath(sift.__file__)), kernel + ".cl")
+    kernel_src = open(kernel_file).read()
+    program = pyopencl.Program(ctx, kernel_src).build("-D WORKGROUP=%s" % kernels[kernel])
+    kernels[kernel] = program
 
 def gaussian_cpu(sigma, size=None):
     """
@@ -81,7 +83,7 @@ def gaussian_cpu(sigma, size=None):
 
     return g
 
-def gaussian_gpu(sigma, size=None):
+def gaussian_gpu_v1(sigma, size=None):
     """
     Calculate a 1D gaussian using pyopencl. 
     This is the same as scipy.signal.gaussian 
@@ -92,16 +94,43 @@ def gaussian_gpu(sigma, size=None):
     if not size:
         size = int(1 + 8 * sigma)
     g_gpu = pyopencl.array.empty(queue, size, dtype=numpy.float32, order="C")
-    program.gaussian(queue, (size,), (1,),
+    t0 = time.time()
+    evt1 = kernels["preprocess"].gaussian(queue, (size,), (1,),
                                         g_gpu.data,                #__global     float     *data,
                                         numpy.float32(sigma),      #const        float     sigma,
                                         numpy.int32(size))         #const        int     SIZE
-    sum_data = pyopencl.array.sum(g_gpu, dtype=numpy.float32, queue=queue).get()
-    program.divide_cst(queue, (size,), (1,),
+    sum_data = pyopencl.array.sum(g_gpu, dtype=numpy.float32, queue=queue)
+    evt2 = kernels["preprocess"].divide_cst(queue, (size,), (1,),
                                           g_gpu.data,                #__global     float     *data,
-                                          sum_data,                  #const        float     sigma,
+                                          sum_data.data,                  #const        float     sigma,
                                           numpy.int32(size))         #const        int     SIZE
-    return g_gpu.get()
+    g = g_gpu.get()
+    logger.info("execution time: %.3fms; Kernel took %.3fms and %.3fms" % (1e3 * (time.time() - t0), 1e-6 * (evt1.profile.end - evt1.profile.start)
+                , 1e-6 * (evt2.profile.end - evt2.profile.start)))
+
+    return g
+
+def gaussian_gpu_v2(sigma, size=None):
+    """
+    Calculate a 1D gaussian using pyopencl. 
+    This is the same as scipy.signal.gaussian.
+    Only one kernel to   
+    
+    @param sigma: width of the gaussian
+    @param size: can be calculated as 1 + 2 * 4sigma  
+    """
+    if not size:
+        size = int(1 + 8 * sigma)
+    g_gpu = pyopencl.array.empty(queue, size, dtype=numpy.float32, order="C")
+    t0 = time.time()
+    evt = kernels["gaussian"].gaussian(queue, (64,), (64,),
+                                        g_gpu.data,                #__global     float     *data,
+                                        numpy.float32(sigma),      #const        float     sigma,
+                                        numpy.int32(size))         #const        int     SIZE
+    g = g_gpu.get()
+    logger.info("execution time: %.3fms; Kernel took %.3fms" % (1e3 * (time.time() - t0), 1e-6 * (evt.profile.end - evt.profile.start)))
+    return g
+
 
 def show(ref, res, delta):
     import pylab
@@ -113,8 +142,8 @@ def show(ref, res, delta):
     pylab.show()
     raw_input("enter")
 
-class test_gaussian(unittest.TestCase):
-    
+class test_gaussian_v1(unittest.TestCase):
+
     def test_odd(self):
         """
         test odd kernel size
@@ -122,7 +151,7 @@ class test_gaussian(unittest.TestCase):
         sigma = 3.0
         size = 27
         ref = gaussian_cpu(sigma, size)
-        res = gaussian_gpu(sigma, size)
+        res = gaussian_gpu_v1(sigma, size)
         delta = ref - res
         if PROFILE:
             show (ref, res, delta)
@@ -135,7 +164,35 @@ class test_gaussian(unittest.TestCase):
         sigma = 3.0
         size = 28
         ref = gaussian_cpu(sigma, size)
-        res = gaussian_gpu(sigma, size)
+        res = gaussian_gpu_v1(sigma, size)
+        delta = ref - res
+        if PROFILE:
+            show (ref, res, delta)
+        self.assert_(abs(ref - res).max() < 1e-6, "gaussian are the same ")
+
+class test_gaussian_v2(unittest.TestCase):
+
+    def test_odd(self):
+        """
+        test odd kernel size
+        """
+        sigma = 3.0
+        size = 27
+        ref = gaussian_cpu(sigma, size)
+        res = gaussian_gpu_v2(sigma, size)
+        delta = ref - res
+        if PROFILE:
+            show (ref, res, delta)
+        self.assert_(abs(ref - res).max() < 1e-6, "gaussian are the same ")
+
+    def test_even(self):
+        """
+        test odd kernel size
+        """
+        sigma = 3.0
+        size = 28
+        ref = gaussian_cpu(sigma, size)
+        res = gaussian_gpu_v2(sigma, size)
         delta = ref - res
         if PROFILE:
             show (ref, res, delta)
@@ -144,8 +201,10 @@ class test_gaussian(unittest.TestCase):
 
 def test_suite_gaussian():
     testSuite = unittest.TestSuite()
-    testSuite.addTest(test_gaussian("test_odd"))
-    testSuite.addTest(test_gaussian("test_even"))
+    testSuite.addTest(test_gaussian_v1("test_odd"))
+    testSuite.addTest(test_gaussian_v1("test_even"))
+    testSuite.addTest(test_gaussian_v2("test_odd"))
+    testSuite.addTest(test_gaussian_v2("test_even"))
     return testSuite
 
 if __name__ == '__main__':
