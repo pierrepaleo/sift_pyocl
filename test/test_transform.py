7b2bd77988ce9f434ffaa24cf7be92a5645e6490
Pierre Paleo <paleo@linkarkouli>
2013-07-24 11:29:45 +0200

    Transformation kernel

---
+++ test/test_transform.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#    Project: Sift implementation in Python + OpenCL
+#             https://github.com/kif/sift_pyocl
+#
+
+"""
+Test suite for transformation kernel
+"""
+
+from __future__ import division
+
+__authors__ = ["Jérôme Kieffer"]
+__contact__ = "jerome.kieffer@esrf.eu"
+__license__ = "BSD"
+__copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+__date__ = "2013-05-28"
+__license__ = """
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+import time, os, logging
+import numpy
+import pyopencl, pyopencl.array
+import scipy, scipy.misc, scipy.ndimage, pylab
+import sys
+import unittest
+from utilstest import UtilsTest, getLogger, ctx
+from test_image_functions import * #for Python implementation of tested functions
+from test_image_setup import *
+import sift
+from sift.utils import calc_size
+logger = getLogger(__file__)
+if logger.getEffectiveLevel() <= logging.INFO:
+    PROFILE = True
+    queue = pyopencl.CommandQueue(ctx, properties=pyopencl.command_queue_properties.PROFILING_ENABLE)
+    import pylab
+else:
+    PROFILE = False
+    queue = pyopencl.CommandQueue(ctx)
+
+SHOW_FIGURES = False
+USE_CPU = False
+
+
+
+print "working on %s" % ctx.devices[0].name
+
+
+
+
+
+class test_transform(unittest.TestCase):
+    def setUp(self):
+        
+        kernel_path = os.path.join(os.path.dirname(os.path.abspath(sift.__file__)), "transform.cl")
+        kernel_src = open(kernel_path).read()
+        self.program = pyopencl.Program(ctx, kernel_src).build() #.build('-D WORKGROUP_SIZE=%s' % wg_size)
+        self.wg = (1, 128)
+
+
+
+    def tearDown(self):
+        self.program = None
+        
+        
+
+    def test_transform(self):
+        '''
+        tests transform kernel
+        '''    
+#        image = scipy.misc.imread(os.path.join("../../test_images/","esrf_grenoble.jpg"),flatten=True).astype(numpy.float32)
+        image = scipy.misc.lena().astype(numpy.float32)
+        image2 = numpy.zeros((724,724),dtype=numpy.float32)
+        image2[106:-106,106:-106] = image
+        
+        #transformation
+        angle = numpy.pi/4.0
+        param1 = numpy.array([numpy.cos(angle),-numpy.sin(angle),0.0],dtype=numpy.float32)
+        param2 = numpy.array([numpy.sin(angle),numpy.sin(angle),0.0],dtype=numpy.float32)
+        fill_value = numpy.float32(0.0)
+        mode = numpy.int32(0)
+        image_height, image_width = image2.shape
+        
+        if (USE_CPU): wg = 1,1
+        else: wg = 1,1
+        shape = calc_size((image_height,image_width), self.wg)
+        
+        gpu_image = pyopencl.array.to_device(queue, image2)
+        gpu_output = pyopencl.array.empty(queue, (image_height, image_width), dtype=numpy.float32, order="C")
+        gpu_param1 = pyopencl.array.to_device(queue,param1)
+        gpu_param2 = pyopencl.array.to_device(queue,param2)
+        image_height, image_width = numpy.int32((image_height, image_width))
+        
+        t0 = time.time()
+        k1 = self.program.transform(queue, shape, wg,
+        		gpu_image.data, gpu_output.data, gpu_param1.data, gpu_param2.data, 
+        		image_width, image_height, fill_value, mode)
+        res = gpu_output.get()
+        t1 = time.time()
+        print res[0,0]
+        
+        ref = scipy.ndimage.interpolation.rotate(image,numpy.degrees(angle))
+        t2 = time.time()
+        
+        SHOW_FIGURES = True
+        if SHOW_FIGURES:
+            fig = pylab.figure()
+            sp1 = fig.add_subplot(121)
+            sp1.imshow(res, interpolation="nearest")
+            sp2 = fig.add_subplot(122)
+            sp2.imshow(ref, interpolation="nearest")
+            fig.show()
+            raw_input("enter")
+
+
+        if PROFILE:
+            logger.info("Global execution time: CPU %.3fms, GPU: %.3fms." % (1000.0 * (t2 - t1), 1000.0 * (t1 - t0)))
+            logger.info("Transformation took %.3fms" % (1e-6 * (k1.profile.end - k1.profile.start)))
+            
+            
+            
+            
+
+def test_suite_transform():
+    testSuite = unittest.TestSuite()
+    testSuite.addTest(test_transform("test_transform"))
+    return testSuite
+
+if __name__ == '__main__':
+    mysuite = test_suite_transform()
+    runner = unittest.TextTestRunner()
+    if not runner.run(mysuite).wasSuccessful():
+        sys.exit(1)
+
