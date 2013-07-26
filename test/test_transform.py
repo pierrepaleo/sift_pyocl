152e8b1e78d528ec6fa601b51682bc76103b1302
Pierre Paleo <paleo@linkarkouli>
2013-07-26 11:13:09 +0200

    Image correction OK for rotation

--- test/test_transform.py
+++ test/test_transform.py
@@ -61,8 +61,7 @@ else:
     queue = pyopencl.CommandQueue(ctx)
 
 SHOW_FIGURES = False
-USE_CPU = False
-
+IMAGE_RESHAPE = False
 
 
 print "working on %s" % ctx.devices[0].name
@@ -84,33 +83,84 @@ class test_transform(unittest.TestCase):
     def tearDown(self):
         self.program = None
         
-        
+
+    def matching_correction(self,image,image2):
+        '''
+        Computes keypoints for two images and try to align image2 on image1
+        '''
+        #computing keypoints matching
+        s = sift.SiftPlan(template=image,devicetype="gpu")
+        kp1 = s.keypoints(image)
+        kp2 = s.keypoints(image2) #image2 and image must have the same size
+        m = sift.MatchPlan(devicetype="GPU")
+        matching = m.match(kp2,kp1)
+        N = matching.shape[0]
+        #solving normals equations for least square fit
+        X = numpy.zeros((2*N,6))
+        X[::2,2:] = 1,0,0,0
+        X[::2,0] = matching.x[:,0]
+        X[::2,1] = matching.y[:,0]
+        X[1::2,0:3] = 0,0,0
+        X[1::2,3] = matching.x[:,0]
+        X[1::2,4] = matching.y[:,0]
+        X[1::2,5] = 1
+        y = numpy.zeros((2*N,1))
+        y[::2,0] = matching.x[:,1]
+        y[1::2,0] = matching.y[:,1]
+        #A = numpy.dot(X.transpose(),X)
+        #sol = numpy.dot(numpy.linalg.inv(A),numpy.dot(X.transpose(),y))
+        sol = numpy.dot(numpy.linalg.pinv(X),y)
+        return sol
+
+
+
+
+
 
     def test_transform(self):
         '''
         tests transform kernel
         '''    
-#        image = scipy.misc.imread(os.path.join("../../test_images/","esrf_grenoble.jpg"),flatten=True).astype(numpy.float32)
-        image = scipy.misc.lena().astype(numpy.float32)
-        image = numpy.ascontiguousarray(image[0:410,0:352])
-        
-        image_height, image_width = image.shape
-        output_height, output_width = int(image_height*numpy.sqrt(2)), int(image_width*numpy.sqrt(2))
 
+        #original image
+        image = scipy.misc.lena().astype(numpy.float32)
+        image = numpy.ascontiguousarray(image[0:512,0:512])
+        image_height, image_width = image.shape
+        
+        IMAGE_RESHAPE = True
+        if IMAGE_RESHAPE:
+            image3_height, image3_width = int(1024), int(1024) #choose the re-scaled size
+            image3 = numpy.zeros((image3_height,image3_width),dtype=numpy.float32)
+            d1 = (image3_width - image_width)/2
+            d0 = (image3_height - image_height)/2
+            image3[d0:-d0,d1:-d1] = numpy.copy(image)
+            image = image3
+            image_height, image_width = image.shape
+            output_height, output_width = image.shape
+       
+        else: output_height, output_width = int(image_height*numpy.sqrt(2)),int(image_width*numpy.sqrt(2))#TODO: automatic calculation
         #transformation
         angle = 0.35 #numpy.pi/5.0
-#        matrix = numpy.array([[numpy.cos(angle),-numpy.sin(angle)],[numpy.sin(angle),numpy.cos(angle)]],dtype=numpy.float32)
-        matrix = numpy.array([[2.0,-1.5],[0.7,2.0]],dtype=numpy.float32)
-        #important for float4
-        matrix_for_gpu = matrix.reshape(4,1)
+        matrix = numpy.array([[numpy.cos(angle),-numpy.sin(angle)],[numpy.sin(angle),numpy.cos(angle)]],dtype=numpy.float32)
+#        matrix = numpy.array([[2.0,-1.5],[0.7,2.0]],dtype=numpy.float32)
         offset_value = numpy.array([-0.0, -0.0],dtype=numpy.float32)
         fill_value = numpy.float32(0.0)
         mode = numpy.int32(0)
+        image2 = scipy.ndimage.interpolation.affine_transform(image,matrix,offset=offset_value,order=1, mode="constant")
+        
+        #perform correction by least square
+        sol = self.matching_correction(image,image2)
+        
+        correction_matrix = numpy.zeros((2,2),dtype=numpy.float32)
+        correction_matrix[0] = sol[0:2,0]
+        correction_matrix[1] = sol[3:5,0]
+        matrix_for_gpu = correction_matrix.reshape(4,1) #for float4 struct
+        offset_value[0] = sol[2,0]
+        offset_value[1] = sol[5,0]
         
         wg = 1,1
         shape = calc_size((output_width,output_height), self.wg)
-        
-        gpu_image = pyopencl.array.to_device(queue, image)
+        gpu_image = pyopencl.array.to_device(queue, image2)
         gpu_output = pyopencl.array.empty(queue, (output_height, output_width), dtype=numpy.float32, order="C")
         gpu_matrix = pyopencl.array.to_device(queue,matrix_for_gpu)
         gpu_offset = pyopencl.array.to_device(queue,offset_value)
@@ -119,34 +169,38 @@ class test_transform(unittest.TestCase):
         
         t0 = time.time()
         k1 = self.program.transform(queue, shape, wg,
-        		gpu_image.data, gpu_output.data, gpu_matrix.data, gpu_offset.data, 
-        		image_width, image_height, output_width, output_height, fill_value, mode)
+                gpu_image.data, gpu_output.data, gpu_matrix.data, gpu_offset.data, 
+                image_width, image_height, output_width, output_height, fill_value, mode)
         res = gpu_output.get()
         t1 = time.time()
-
-        ref = scipy.ndimage.interpolation.affine_transform(image,matrix,
-        	offset=offset_value, output_shape=(output_height,output_width), 
-        	order=1, mode="constant", cval=fill_value)
+        
+        ref = scipy.ndimage.interpolation.affine_transform(image2,correction_matrix,
+        	offset=offset_value, output_shape=(output_height,output_width),order=1, mode="constant", cval=fill_value)
         t2 = time.time()
         
-        delta = abs(res-ref)
-        delta_arg = delta.argmax()
-        delta_max = delta.max()
-        at_0, at_1 = delta_arg/output_width, delta_arg%output_width
-        print("Max error: %f at (%d, %d)" %(delta_max, at_0, at_1))
-        print res[at_0,at_1]
-        print ref[at_0,at_1]
+#        delta = abs(res-ref)
+#        delta_arg = delta.argmax()
+#        delta_max = delta.max()
+#        at_0, at_1 = delta_arg/output_width, delta_arg%output_width
+#        print("Max error: %f at (%d, %d)" %(delta_max, at_0, at_1))
+#        print res[at_0,at_1]
+#        print ref[at_0,at_1]
         
         SHOW_FIGURES = True
         if SHOW_FIGURES:
             fig = pylab.figure()
-            sp1 = fig.add_subplot(221,title="Output")
-            sp1.imshow(res, interpolation="nearest")
-            sp2 = fig.add_subplot(222,title="Reference")
+            sp1 = fig.add_subplot(221,title="Input image")
+            sp1.imshow(image, interpolation="nearest")
+            sp2 = fig.add_subplot(222,title="Image after deformation")
+            sp2.imshow(image2, interpolation="nearest")
+            sp2 = fig.add_subplot(223,title="Corrected image (OpenCL)")
+            sp2.imshow(res, interpolation="nearest")
+            sp2 = fig.add_subplot(224,title="Corrected image (Scipy)")
             sp2.imshow(ref, interpolation="nearest")
-            sp3 = fig.add_subplot(223,title="delta (max = %f)" %delta_max)
-            sh3 = sp3.imshow(delta[:,:], interpolation="nearest")
-            cbar = fig.colorbar(sh3)
+#            sp2.imshow(ref, interpolation="nearest")
+#            sp3 = fig.add_subplot(223,title="delta (max = %f)" %delta_max)
+#            sh3 = sp3.imshow(delta[:,:], interpolation="nearest")
+#            cbar = fig.colorbar(sh3)
             fig.show()
             raw_input("enter")
 
@@ -155,6 +209,16 @@ class test_transform(unittest.TestCase):
             logger.info("Global execution time: CPU %.3fms, GPU: %.3fms." % (1000.0 * (t2 - t1), 1000.0 * (t1 - t0)))
             logger.info("Transformation took %.3fms" % (1e-6 * (k1.profile.end - k1.profile.start)))
             
+
+
+
+
+
+
+
+
+
+
             
             
             
